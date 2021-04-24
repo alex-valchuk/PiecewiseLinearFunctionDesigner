e866cdbe622acbd5ae1744c071bc68beb0d23250
Alex Valchuk <alex.valchuk@deltatre.it>
2021-04-24 22:45:33 +0300

    improved functions visual

--- src/PiecewiseLinearFunctionDesigner.Localization/RussianTextLocalization.cs
+++ src/PiecewiseLinearFunctionDesigner.Localization/RussianTextLocalization.cs
@@ -3,7 +3,7 @@
     public class RussianTextLocalization : ITextLocalization
     {
         public string AppName => "Дизайнер кривых линий";
-        public string Functions => "Функции";
+        public string Functions => "Список функций проекта";
         public string Function => "Функция";
         public string Add => "Добавить";
         public string FunctionWithNameAlreadyAdded => "Функция с именем '{0}' уже существует";
